05abdb3dab5e70ed22abca99b6ea634c81ecb320
Victor Machado <victormachadogp@gmail.com>
2021-08-28 23:20:39 -0300

    equipmentPositionHistory adicionado no mapa

--- src/components/EquipmentMap.vue
+++ src/components/EquipmentMap.vue
@@ -1,10 +1,28 @@
 <template>
-  <div class="container">
-    <l-map class="map" :zoom="zoom" :center="center" :options="mapOptions">
+  <div class="container" style="height: 600px; width: 100%">
+    <div style="height: 200px; overflow: auto">
+      <p>First marker is placed at {{ withPopup.lat }}, {{ withPopup.lng }}</p>
+      <p>Center is at {{ currentCenter }} and the zoom is: {{ currentZoom }}</p>
+    </div>
+    <l-map
+      class="map"
+      :zoom="zoom"
+      :center="center"
+      :options="mapOptions"
+      style="height: 80%"
+      @update:center="centerUpdate"
+      @update:zoom="zoomUpdate"
+    >
       <l-tile-layer :url="url" :attribution="attribution" />
-      <l-marker :lat-lng="withPopup">
+      <l-marker
+        v-for="(equipment, index) in jsonData"
+        :key="index"
+        :lat-lng="
+          latLgn(equipment.positions[index].lat, equipment.positions[index].lon)
+        "
+      >
         <l-popup>
-          <div>I am a popup</div>
+          <div @click="innerClick">I am a popup</div>
         </l-popup>
       </l-marker>
     </l-map>
@@ -27,19 +45,41 @@ export default {
   data() {
     return {
       jsonData,
-      zoom: 13,
-      center: latLng(47.41322, -1.219482),
+      zoom: 11,
+      center: latLng(-19.066661, -45.96405),
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      withPopup: latLng(47.41422, -1.219482),
+      withPopup: latLng(-19.126536, -45.947756),
       currentZoom: 13,
-      currentCenter: latLng(47.41422, -1.219482),
+      currentCenter: latLng(-19.126536, -45.947756),
       showParagraph: false,
       mapOptions: {
         zoomSnap: 0.5,
       },
     };
+  },
+  methods: {
+    latLgn(lat, lng) {
+      return latLng(lat, lng);
+    },
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom;
+
+      const data = this.jsonData.map((item) => {
+        let lat = item.positions[0].lat;
+        let lon = item.positions[0].lon;
+        console.log(`The lat is: ${lat} and the long is: ${lon}`);
+      });
+
+      console.log(data);
+    },
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    innerClick() {
+      alert("Click!");
+    },
   },
 };
 </script>
