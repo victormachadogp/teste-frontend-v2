4430e57b13e8ba7a6a00a856108c2c4ab2868b9e
Victor Machado <victormachadogp@gmail.com>
2021-09-01 10:53:24 -0300

    Componente criado - Historico de estados

--- src/components/EquipmentMap.vue
+++ src/components/EquipmentMap.vue
@@ -30,30 +30,8 @@
       </l-map>
     </div>
 
-    <div class="mb-10 lg:mb-0 lg:order-2" >
-        <!-- <p>First marker is placed at {{ withPopup.lat }}, {{ withPopup.lng }}</p>
-        <p>Center is at {{ currentCenter }} and the zoom is: {{ currentZoom }}</p> -->
-        
-        <div class="max-h-48 sm:max-h-96 w-full lg:w-96 overflow-y-auto py-3 scroll-style rounded-md bg-white border history-container">
-        <h2 class="text-center text-2xl font-medium text-gray-600 ">Histórico</h2>
-        <div class="border-b border-gray-300 my-3"></div>
-          <p v-if="!ishistoryPositionOpen" class="text-xs mt-5 text-gray-500 text-center italic">Clique em um marcador no mapa para visualizar o histórico.</p>
-
-          <div class="my-4 text-center max-w-xs mx-auto v-for-container bg-white border shadow-md " v-for="(item, index) in currentStatePositionsHistory" :key="index">
-            <p class="text-sm py-1 text-gray-600">{{ item.date }}</p>
-            <div class="flex justify-between">
-              <div class="flex items-center">
-                <div class="text-small text-white bg-variant-color-1 flex items-center justify-center rounded-bl-md w-10 h-7">LAT</div>
-                <span class="text-sm ml-3">{{ item.lat }}</span>
-              </div>
-              <div class="flex items-center">
-                <span class="text-sm mr-3">{{ item.lon }}</span>
-                <div class="text-small text-white bg-variant-color-2 flex items-center justify-center rounded-br-md w-10 h-7">LON</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+   <state-history :isHistoryOpen="isHistoryPositionOpen" :currentcurrentStatePositions="currentStatePositionsHistory"/>
+   
   </div>
 
 </template>
@@ -66,6 +44,7 @@ import { LMap, LTileLayer, LMarker, LPopup } from "vue2-leaflet";
 import equipmentPositionHistory from "../static/data/equipmentPositionHistory.json";
 import equipmentStateHistory from "../static/data/equipmentStateHistory.json";
 import equipmentState from "../static/data/equipmentState.json";
+import StateHistory from "./StateHistory.vue";
 
 export default {
   name: "EquipmentMap",
@@ -74,6 +53,7 @@ export default {
     LTileLayer,
     LMarker,
     LPopup,
+    StateHistory,
   },
   data() {
     return {
@@ -85,7 +65,7 @@ export default {
       },
       // Mude isso
       currentEquipmentStateId: null,
-      ishistoryPositionOpen: false,
+      isHistoryPositionOpen: false,
       currentStatePositionsHistory: null,
       equipmentStateInfo: null,
       zoom: 11,
@@ -93,6 +73,7 @@ export default {
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      // Apagar esses 3 caras se não for usar
       withPopup: latLng(-19.126536, -45.947756),
       currentZoom: 11,
       currentCenter: latLng(-19.126536, -45.947756),
@@ -113,7 +94,7 @@ export default {
     },
     innerClick() {},
     showState(index) {
-      this.ishistoryPositionOpen = true;
+      this.isHistoryPositionOpen = true;
 
       // Mostra os dados no historico
       this.currentStatePositionsHistory =
@@ -164,47 +145,5 @@ export default {
   background-color: gray;
   border-radius: 50%;
   margin: 0 auto;
-}
-
-.text-small {
-  font-size: 0.65rem;
-}
-
-.bg-variant-color-1 {
-  background-color: #52bbdc;
-}
-
-.bg-variant-color-2 {
-  background-color: #3f90cb;
-}
-
-.scroll-style::-webkit-scrollbar {
-  width: 0.5em;
-}
-
-.scroll-style::-webkit-scrollbar-track {
-  background-color: rgb(197, 197, 197);
-}
-
-.scroll-style::-webkit-scrollbar-thumb {
-  background-color: #52bbdc;
-}
-
-.history-container {
-  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
-  border-radius: 5px;
-}
-
-@media (max-width: 390x) {
-  .v-for-container {
-    margin-left: 10px;
-    margin-right: 10px;
-  }
-}
-
-@media (min-width: 1024px) {
-  .history-container {
-    min-height: 600px !important;
-  }
 }
 </style>
---
+++ src/components/StateHistory.vue
@@ -0,0 +1,85 @@
+<template>
+     <div class="mb-10 lg:mb-0 lg:order-2" >   
+
+         <!-- <p>First marker is placed at {{ withPopup.lat }}, {{ withPopup.lng }}</p>
+        <p>Center is at {{ currentCenter }} and the zoom is: {{ currentZoom }}</p> --> 
+            
+        <div class="max-h-48 sm:max-h-96 w-full lg:w-96 overflow-y-auto py-3 scroll-style rounded-md bg-white border history-container">
+        <h2 class="text-center text-2xl font-medium text-gray-600 ">Histórico</h2>
+        <div class="border-b border-gray-300 my-3"></div>
+          <p v-if="!isHistoryOpen" class="text-xs mt-5 text-gray-500 text-center italic">Clique em um marcador no mapa para visualizar o histórico.</p>
+          
+
+          <div class="my-4 text-center max-w-xs mx-auto v-for-container rounded-md bg-white border shadow-md " v-for="(item, index) in currentcurrentStatePositions" :key="index">
+            <p class="text-sm py-1 text-gray-600">{{ item.date }}</p>
+            <div class="flex justify-between">
+              <div class="flex items-center">
+                <div class="text-small text-white bg-variant-color-1 flex items-center justify-center rounded-bl-md w-10 h-7">LAT</div>
+                <span class="text-sm ml-3">{{ item.lat }}</span>
+              </div>
+              <div class="flex items-center">
+                <span class="text-sm mr-3">{{ item.lon }}</span>
+                <div class="text-small text-white bg-variant-color-2 flex items-center justify-center rounded-br-md w-10 h-7">LON</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentcurrentStatePositions: {
+      type: Array,
+    },
+    isHistoryOpen: {
+      type: Boolean,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.text-small {
+  font-size: 0.65rem;
+}
+
+.bg-variant-color-1 {
+  background-color: #52bbdc;
+}
+
+.bg-variant-color-2 {
+  background-color: #3f90cb;
+}
+
+.scroll-style::-webkit-scrollbar {
+  width: 0.5em;
+}
+
+.scroll-style::-webkit-scrollbar-track {
+  background-color: rgb(197, 197, 197);
+}
+
+.scroll-style::-webkit-scrollbar-thumb {
+  background-color: #52bbdc;
+}
+
+.history-container {
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+}
+
+@media (max-width: 390x) {
+  .v-for-container {
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+}
+
+@media (min-width: 1024px) {
+  .history-container {
+    min-height: 600px !important;
+  }
+}
+</style>
